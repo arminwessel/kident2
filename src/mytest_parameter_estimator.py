35070e25c798e76ca7507ca208b22155c87cfe1e
Armin Wessel <armin.wessel95@gmail.com>
2022-11-11 09:31:57 +0100

    New dh estimation working with all parameter types if data is perfect results with real data are not so good

--- src/mytest_parameter_estimator.py
+++ src/mytest_parameter_estimator.py
@@ -16,10 +16,15 @@ r_nom = ParameterEstimator.dhparams["r_nom"].astype(float)
 d_nom = ParameterEstimator.dhparams["d_nom"].astype(float)
 alpha_nom = ParameterEstimator.dhparams["alpha_nom"].astype(float)
 
-# r_nom[3] = r_nom[3] + 0.006
-# theta_nom[4] = theta_nom[4] + 0.0003
-# alpha_nom[6] = alpha_nom[6] + 0.0007
-d_nom[5] = d_nom[5] + 0.005
+theta_error = np.array([0, 0, 0, 0, 0, - 0.0003, 0])
+r_error = np.array([0, 0, 0, 0.004, 0, 0, 0])
+d_error = np.array([0, 0, 0, 0.005, 0, 0, 0])
+alpha_error = np.array([0, 0.0007, 0, 0, 0, 0, 0])
+
+r_nom = r_nom + r_error
+theta_nom = theta_nom + theta_error
+alpha_nom = alpha_nom + alpha_error
+d_nom = d_nom + d_error
 
 observations_file_str = "observations_fake.p"
 observations_file = open(observations_file_str, 'rb')
@@ -71,9 +76,11 @@ for markerid in list(observations)[:]:
         # perform necessary inversions
         T_C7 = np.linalg.inv(T_7C)
         T_MC_2 = np.linalg.inv(T_CM_2)
-        T_70_2 = np.linalg.inv(T_07_2)
+        T_70_1 = np.linalg.inv(T_07_1)
 
-        delta_D = T_7C @ T_CM_1 @ T_MC_2 @ T_C7 @ T_70_2 @ T_07_1 - np.eye(4)
+        D_meas = T_7C @ T_CM_1 @ T_MC_2 @ T_C7
+        D_nom = T_70_1 @ T_07_2
+        delta_D = D_meas @ np.linalg.inv(D_nom) - np.eye(4)
         drvec, _ = cv2.Rodrigues(delta_D[0:3, 0:3] + np.eye(3))
         drvec = drvec.flatten()
         # drvec = np.array([delta_D[2, 1], delta_D[0, 2], delta_D[1, 0]])
@@ -81,17 +88,19 @@ for markerid in list(observations)[:]:
         pose_error = np.concatenate((dtvec, drvec))
 
         # calculate the corresponding difference jacobian
-        jacobian = pe.get_parameter_jacobian_dual(q1=q1, q2=q2,
+        jacobian = pe.get_parameter_jacobian_dual_2(q1=q1, q2=q2,
                                                   theta_all=pe.theta_nom,
                                                   d_all=pe.d_nom,
                                                   r_all=pe.r_nom,
                                                   alpha_all=pe.alpha_nom)
 
         # calculate position error in data
-        diff = np.array([(T_07_1 @ T_7C @ T_CM_1)[0, 3] - (T_07_2 @ T_7C @ T_CM_2)[0, 3],
+        diff = np.linalg.norm(np.array([(T_07_1 @ T_7C @ T_CM_1)[0, 3] - (T_07_2 @ T_7C @ T_CM_2)[0, 3],
                          (T_07_1 @ T_7C @ T_CM_1)[1, 3] - (T_07_2 @ T_7C @ T_CM_2)[1, 3],
-                         (T_07_1 @ T_7C @ T_CM_1)[2, 3] - (T_07_2 @ T_7C @ T_CM_2)[2, 3]])
-        diff_k.append(np.linalg.norm(diff))
+                         (T_07_1 @ T_7C @ T_CM_1)[2, 3] - (T_07_2 @ T_7C @ T_CM_2)[2, 3]]))
+        if diff > 0.02:
+            continue
+        diff_k.append(diff)
 
         pe.rls.add_obs(S=jacobian, Y=pose_error)
         estimate_k = pe.rls.get_estimate().flatten()
@@ -181,5 +190,33 @@ for elem in estimate_k:
 
 plt.figure('diffs')
 plt.plot(diff_k)
+mean = np.mean(diff_k)
+print(f"mean diff = {mean}")
+
+fig_curr_est, ax_curr_est = plt.subplots(2, 2)
+n = 7
+X = [e for e in range(0, n)]
+axis = ax_curr_est
+param_errors = estimates_k
+axis[0, 0].clear()
+Y = param_errors[0:n, -1]
+axis[0, 0].stem(X, Y)
+axis[0, 0].set_title(r'$\Delta$$\theta$')
+
+axis[0, 1].clear()
+Y = param_errors[n:2*n, -1]
+axis[0, 1].stem(X, Y)
+axis[0, 1].set_title(r'$\Delta$d')
+
+axis[1, 0].clear()
+Y = param_errors[2*n:3*n, -1]
+axis[1, 0].stem(X, Y)
+axis[1, 0].set_title(r'$\Delta$r')
+
+axis[1, 1].clear()
+Y = param_errors[3*n:4*n, -1]
+axis[1, 1].stem(X, Y)
+axis[1, 1].set_title(r'$\Delta$$\alpha$')
+
 plt.show()
 print("done")
