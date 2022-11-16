jacobian is wrong for difference, we need a Jacobian form for dT (can be substracted) instead of deltaT

src/mytest_parameter_estimator_difference.py
@@ -10,28 +10,31 @@ from itertools import combinations
 from pytransform3d import rotations as pr
 from pytransform3d import transformations as pt
 from pytransform3d.transform_manager import TransformManager
+from scipy.spatial.transform import Rotation
+
+
 
 theta_nom = ParameterEstimator.dhparams["theta_nom"].astype(float)
 r_nom = ParameterEstimator.dhparams["r_nom"].astype(float)
 d_nom = ParameterEstimator.dhparams["d_nom"].astype(float)
 alpha_nom = ParameterEstimator.dhparams["alpha_nom"].astype(float)
 
-theta_error = np.array([0, 0, 0, 0, 0, 0, 0])
-r_error = np.array([0, 0, 0, 0, 0, 0, 0])
-d_error = np.array([0, 0, 0, 0, 0, 0, 0])
-alpha_error = np.array([0, 0, 0, 0, 0, 0, 0])
+theta_error = np.array([0.0, 0, 0, 0, 0, 0, 0])
+r_error = np.array([0.0, 0, 0, 0, 0.05, 0, 0])
+d_error = np.array([0.0, 0, 0, 0, 0, 0, 0])
+alpha_error = np.array([0.0, 0, 0, 0, 0, 0, 0])
 
 
-r_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.1 cm
-d_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.1 cm
-alpha_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.001 rad = 0.0573 deg
-theta_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.001 rad = 0.0573 deg
+# r_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.1 cm
+# d_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.1 cm
+# alpha_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.001 rad = 0.0573 deg
+# theta_error = np.random.uniform(low=-0.001, high=0.001, size=(7,))  # max error: 0.001 rad = 0.0573 deg
 
 
-r_nom = r_nom + r_error
-theta_nom = theta_nom + theta_error
-alpha_nom = alpha_nom + alpha_error
-d_nom = d_nom + d_error
+r_real = r_nom + r_error
+theta_real = theta_nom + theta_error
+alpha_real = alpha_nom + alpha_error
+d_real = d_nom + d_error
 
 observations_file_str = "observations_fake.p"
 observations_file = open(observations_file_str, 'rb')
@@ -70,38 +73,47 @@ for markerid in list(observations)[:]:
         if num_observed > 50:
             continue
 
-        # extract measurements
         q1 = np.array(obs1["q"])
         q2 = np.array(obs2["q"])
         T_CM_1 = T_corr @ utils.H_rvec_tvec(obs1["rvec"], obs1["tvec"])
         T_CM_2 = T_corr @ utils.H_rvec_tvec(obs2["rvec"], obs2["tvec"])
+        T_07_1 = pe.get_T_jk(0, 7, q1, theta_real, d_real, r_real, alpha_real)
+        T_07_2 = pe.get_T_jk(0, 7, q2, theta_real, d_real, r_real, alpha_real)
 
-        # calculate nominal transforms
-        T_07_1 = pe.get_T_jk(0, 7, q1, theta_nom, d_nom, r_nom, alpha_nom)
-        T_07_2 = pe.get_T_jk(0, 7, q2, theta_nom, d_nom, r_nom, alpha_nom)
+    # expected motion of camera center in C1 frame is
+        T_nom = np.linalg.inv(T_07_1 @ T_7C) @ (T_07_2 @ T_7C)
+        t_nom = T_nom[0:3, 3]
+        rot_nom = Rotation.from_dcm(T_nom[0:3, 0:3])
+        r_euler_nom = rot_nom.as_euler("zyx", degrees=True)
+    # measured motion of camera center in C1 frame is
+        T_meas = T_CM_1 @ np.linalg.inv(T_CM_2)
+        t_meas = T_meas[0:3, 3]
+        rot_meas = Rotation.from_dcm(T_meas[0:3, 0:3])
+        r_euler_meas = rot_meas.as_euler("zyx", degrees=True)
+    # error in measurement and nominal transformation is
+        rot_error = r_euler_meas - r_euler_nom
+        t_error = t_meas - t_nom
+        rot_error = Rotation.from_euler("zyx", rot_error, degrees=True)
+        R_error = rot_error.as_dcm()
+        delta = cv2.Rodrigues(R_error[0:3, 0:3])[0].flatten()
+    # composed error vector
+        pose_error = np.concatenate((t_error, delta))
 
-        # perform necessary inversions
-        T_C7 = np.linalg.inv(T_7C)
-        T_MC_2 = np.linalg.inv(T_CM_2)
-        T_70_1 = np.linalg.inv(T_07_1)
+        # calculate the corresponding jacobians
+        jacobian1 = pe.get_parameter_jacobian(q=q1,
+                                              theta_all=pe.theta_nom,
+                                              d_all=pe.d_nom,
+                                              r_all=pe.r_nom,
+                                              alpha_all=pe.alpha_nom)
+        jacobian2 = pe.get_parameter_jacobian(q=q2,
+                                              theta_all=pe.theta_nom,
+                                              d_all=pe.d_nom,
+                                              r_all=pe.r_nom,
+                                              alpha_all=pe.alpha_nom)
+    # difference of jacobians
+        jacobian = jacobian1 + jacobian2
 
-        D_meas = T_7C @ T_CM_1 @ T_MC_2 @ T_C7
-        D_nom = T_70_1 @ T_07_2
-        delta_D = D_meas @ np.linalg.inv(D_nom) - np.eye(4)
-        drvec, _ = cv2.Rodrigues(delta_D[0:3, 0:3] + np.eye(3))
-        drvec = drvec.flatten()
-        # drvec = np.array([delta_D[2, 1], delta_D[0, 2], delta_D[1, 0]])
-        dtvec = delta_D[0:3, 3]
-        pose_error = np.concatenate((dtvec, drvec))
-
-        # calculate the corresponding difference jacobian
-        jacobian = pe.get_parameter_jacobian_dual_2(q1=q1, q2=q2,
-                                                  theta_all=pe.theta_nom,
-                                                  d_all=pe.d_nom,
-                                                  r_all=pe.r_nom,
-                                                  alpha_all=pe.alpha_nom)
-
-        # calculate position error in data
+    # calculate position error in data
         diff = np.linalg.norm(np.array([(T_07_1 @ T_7C @ T_CM_1)[0, 3] - (T_07_2 @ T_7C @ T_CM_2)[0, 3],
                          (T_07_1 @ T_7C @ T_CM_1)[1, 3] - (T_07_2 @ T_7C @ T_CM_2)[1, 3],
                          (T_07_1 @ T_7C @ T_CM_1)[2, 3] - (T_07_2 @ T_7C @ T_CM_2)[2, 3]]))
@@ -114,6 +126,10 @@ for markerid in list(observations)[:]:
         estimate_k = np.reshape(np.array(estimate_k), (-1, 1))
         estimates_k = np.hstack((estimates_k, estimate_k))
         num_observed += 1
+
+        #tm.plot_frames_in(frame="0")
+        #plt.show()
+        #print("plt")
         # # Measurement
         # T_CM_1 = T_corr @ utils.H_rvec_tvec(obs1["rvec"], obs1["tvec"])
         # T_CM_2 = T_corr @ utils.H_rvec_tvec(obs2["rvec"], obs2["tvec"])

src/parameter_estimator.py
@@ -110,7 +110,7 @@ class ParameterEstimator:
         T_0N = self.get_T_jk(0, num_links, q, theta_all, d_all, r_all, alpha_all)
         t_0N = T_0N[0:3, 3]
         for i in range(num_links):   # iterate over the links of the robot
-            theta = theta_all[i]
+            theta = theta_all[i] + q[i]
             d = d_all[i]
             r = r_all[i]
             alpha = alpha_all[i]
@@ -143,7 +143,7 @@ class ParameterEstimator:
         J = np.zeros((6, 4 * num_links))
         J0 = np.zeros((3, num_links))
         J[0:3, :] = np.concatenate((J5, J2, J3, J6), axis=1)  # upper part of Jacobian is for differential translation
-        J[3:6, :] = np.concatenate((J3, J0, J0, J2), axis=1)  # lower part is for differential rotation
+        #J[3:6, :] = np.concatenate((J3, J0, J0, J2), axis=1)  # lower part is for differential rotation
         return J
 
     def get_parameter_jacobian_dual(self, q1, q2, theta_all, d_all, r_all, alpha_all) -> np.array:
@@ -283,20 +283,20 @@ class ParameterEstimator:
             v_3 = np.array([0, - m.sin(alpha), m.cos(alpha)])
 
             # compute vectors that make up columns of Jacobian
-            j_1 = R @ v_1 + np.cross(t, (R @ v_3))
-            j_2 = R @ v_2
-            j_3 = R @ v_3
-            j_4 = np.cross(t, (R @ v_2))
-            j_5 = np.cross(j_3, t_tot) + j_1
-            j_6 = np.cross(j_2, t_tot) + j_4
+            jf_1 = R @ v_1 + np.cross(t, (R @ v_3))
+            jf_2 = R @ v_2
+            jf_3 = R @ v_3
+            jf_4 = np.cross(t, (R @ v_2))
+            jf_5 = np.cross(jf_3, t_tot) + jf_1
+            jf_6 = np.cross(jf_2, t_tot) + jf_4
 
             # add vectors to columns
-            J1[:, i] += j_1
-            J2[:, i] += j_2
-            J3[:, i] += j_3
-            J4[:, i] += j_4
-            #J5[:, i] += j_5
-            #J6[:, i] += j_6
+            J1[:, i] += jf_1
+            J2[:, i] += jf_2
+            J3[:, i] += jf_3
+            J4[:, i] += jf_4
+            J5[:, i] += jf_5
+            J6[:, i] += jf_6
 
         # calculate the reverse chain
             # parameters for current link
@@ -316,20 +316,20 @@ class ParameterEstimator:
             w_3 = np.array([r * m.sin(theta), r * m.cos(theta), 0])
 
             # compute vectors that make up columns of Jacobian
-            j_1 = np.cross(t, (R @ w_2))
-            j_2 = R @ w_1
-            j_3 = R @ w_2
-            j_4 = np.cross(t, (R @ w_1)) + R @ w_3
-            j_5 = np.cross(j_3, t_tot) + j_1
-            j_6 = np.cross(j_2, t_tot) + j_4
+            jr_1 = np.cross(t, (R @ w_2))
+            jr_2 = R @ w_1
+            jr_3 = R @ w_2
+            jr_4 = np.cross(t, (R @ w_1)) + R @ w_3
+            jr_5 = np.cross(jr_3, t_tot) + jr_1
+            jr_6 = np.cross(jr_2, t_tot) + jr_4
 
             # add vectors to columns
-            J1[:, i] += j_1
-            J2[:, i] += j_2
-            J3[:, i] += j_3
-            J4[:, i] += j_4
-            #J5[:, i] += j_5
-            #J6[:, i] += j_6
+            J1[:, i] += jr_1
+            J2[:, i] += jr_2
+            J3[:, i] += jr_3
+            J4[:, i] += jr_4
+            J5[:, i] += jr_5
+            J6[:, i] += jr_6
 
         J = np.zeros((6, 4 * num_links))
         J0 = np.zeros((3, num_links))

src/visualize_observations.py
@@ -16,7 +16,7 @@ r_nom = ParameterEstimator.dhparams["r_nom"]
 d_nom = ParameterEstimator.dhparams["d_nom"]
 alpha_nom = ParameterEstimator.dhparams["alpha_nom"]
 
-observations_file_str = "observations_fake.p"
+observations_file_str = "observations_big.p"
 observations_file = open(observations_file_str, 'rb')
 
 # dump information to that file
@@ -77,7 +77,7 @@ T_corr = np.array([[ 0,  0, 1, 0],
 
 tm = TransformManager()
 
-for markerid in list(observations)[:7]:
+for markerid in list(observations):
     for id, obs in enumerate(observations[markerid]):
         # T_07 = ParameterEstimator.get_T__i0(7, np.array(obs['q']), d_nom, r_nom, alpha_nom)
 