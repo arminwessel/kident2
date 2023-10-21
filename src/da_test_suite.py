7df1600896c24529d93854857b010238852ac25f
Armin Wessel <armin.wessel95@gmail.com>
2023-10-21 19:35:45 +0200

    experiments are saved automatically

--- src/da_test_suite.py
+++ src/da_test_suite.py
@@ -1,13 +1,6 @@
-import pickle
-import numpy as np
-import matplotlib.pyplot as plt
-from parameter_estimator import ParameterEstimator
-import utils
-from itertools import combinations
-import random
-import time
-from scipy.optimize import least_squares
+import datetime
 from da_test_suite_functions import *
+from exp_data_handler import *
 
 # import nominal parameters
 theta_nom = ParameterEstimator.dhparams["theta_nom"].astype(float)
@@ -18,26 +11,28 @@ nominal_parameters = {'theta': theta_nom, 'd': d_nom, 'r': r_nom, 'alpha': alpha
 
 # define which parameters are to be identified
 parameter_id_masks = dict()
-parameter_id_masks['theta'] = [False, True, True, True, True, True, False, False]
-parameter_id_masks['d'] = [False, True, True, True, True, True, False, False]
-parameter_id_masks['r'] = [False, True, True, True, True, True, False, False]
-parameter_id_masks['alpha'] = [False, True, True, True, True, True, False, False]
+parameter_id_masks['theta'] =   [False, True, True, True, True, True, True, True]
+parameter_id_masks['d'] =       [False, True, True, True, True, True, True, True]
+parameter_id_masks['r'] =       [False, True, True, True, True, True, True, True]
+parameter_id_masks['alpha'] =   [False, True, True, True, True, True, True, True]
 
 # apply the errors - model the real robot
-theta_error = apply_error_to_params(theta_nom, parameter_id_masks['theta'], 0, 'deg_to_rad')
-d_error = apply_error_to_params(d_nom, parameter_id_masks['d'], 0, 'm_to_mm')
-r_error = apply_error_to_params(r_nom, parameter_id_masks['r'], 0, 'm_to_mm')
-alpha_error = apply_error_to_params(alpha_nom, parameter_id_masks['alpha'], 0, 'deg_to_rad')
+factor = 30
+theta_error = apply_error_to_params(theta_nom, parameter_id_masks['theta'], factor, 'deg_to_rad')
+d_error = apply_error_to_params(d_nom, parameter_id_masks['d'], factor, 'm_to_mm')
+r_error = apply_error_to_params(r_nom, parameter_id_masks['r'], factor, 'm_to_mm')
+alpha_error = apply_error_to_params(alpha_nom, parameter_id_masks['alpha'], factor, 'deg_to_rad')
 error_parameters = {'theta': theta_error, 'd': d_error, 'r': r_error, 'alpha': alpha_error}
 
 
 # import observations from file
-observations_file_str_list = ['obs_2007_gazebo_iiwa_stopping.bag_20230720-135812.p',
-                              'observations_fake.p',
-                              'observations_fake_neu.p',
-                              'obs_2007_gazebo_.p',
-                              'observations_simulated_20231010_074809.p']
-observations_file = open(observations_file_str_list[4], 'rb')
+observations_file_str_dict = {1: r'observation_files/obs_2007_gazebo_iiwa_stopping.bag_20230720-135812.p',  # works
+                              4: r'observation_files/obs_2007_gazebo_.p',  # works
+                              9: r'observation_files/observations_simulated_w_error_0mm_0deg_num24020231020_163148.p',
+                              10: r'observation_files/observations_simulated_w_error_0.5mm_0.5deg_num24020231020_164948.p'}
+obsservations_file_select = 9
+observations_file = open(observations_file_str_dict[obsservations_file_select], 'rb')
+
 observations = pickle.load(observations_file)
 observations_file.close()
 
@@ -58,35 +53,54 @@ current_estimate = error_parameters
 current_error_evolution = []
 estimated_params_evolution = []
 estimated_errors_evolution = []
-for i in range(2):
+num_iterations = 8
+for i in range(num_iterations):
     print(f"Iteration {i}")
     # below the observation are describing a robot with nominal parameters, and the identification is given
     # the error parameters - this is to be able to vary the error easily
-    current_errors, estimated_params, list_marker_locations = identify(observations, 50, current_estimate, parameter_id_masks)
-    current_estimate = estimated_params
+    current_errors, current_estimate, list_marker_locations, jac_quality = identify(observations,
+                                                                                    50,
+                                                                                    current_estimate,
+                                                                                    parameter_id_masks)
+    print(f'Jacobian rank: {jac_quality["rank"]}')
     current_error_evolution.append(current_errors)
     estimated_params_evolution.append(current_estimate)
-    estimated_errors_evolution.append(diff_dictionaries(current_estimate, nominal_parameters))
+    estimated_errors = diff_dictionaries(current_estimate, nominal_parameters)
+    estimated_errors_evolution.append(estimated_errors)
 
+
+############################################
 titles = {'suptitle': 'Evolution of current estimate',
           'theta': r'$\theta$ [°]',
           'd': r'd [mm]',
           'r': r'r [mm]',
           'alpha': r'$\alpha$ [°]'}
-plot_evolution(titles, estimated_params_evolution)
-
-titles = {'suptitle': 'Evolution of parameter errors',
+fig_param_evolution = plot_evolution(titles, estimated_params_evolution)
+############################################
+titles = {'suptitle': 'Evolution of residual error',
           'theta': r'$\Delta$$\theta$ [°]',
           'd': r'$\Delta$d [mm]',
           'r': r'$\Delta$r [mm]',
           'alpha': r'$\Delta$$\alpha$ [°]'}
-plot_evolution(titles, estimated_errors_evolution)
+fig_error_evolution = plot_evolution(titles, estimated_errors_evolution)
+############################################
 
-df = result_to_df(error_parameters, current_estimate)
-dataframe_to_pdf(df, 'test_1.pdf')
-with open('marker_locations.p', 'wb') as f:  # open a text file
-    pickle.dump(list_marker_locations, f) # serialize the list
+simulated_errors = diff_dictionaries(nominal_parameters, error_parameters)
+identified_errors = diff_dictionaries(current_estimate, error_parameters)
+identification_accuracy = diff_dictionaries(identified_errors, simulated_errors)
+dataframe = result_to_df(diff_dictionaries(nominal_parameters, error_parameters),
+                         diff_dictionaries(current_estimate, error_parameters))
 
-plt.show()
+exp_handler = ExperimentDataHandler()
+exp_handler.add_figure(fig_error_evolution, 'error_evolution')
+exp_handler.add_figure(fig_param_evolution, 'param_evolution')
+exp_handler.add_note(f"nominal params: \n{ParameterEstimator.dhparams}\n\n" +
+                     f"parameter identification masks\n{parameter_id_masks}\n\n" +
+                     f"error factor\n{factor}\n\n" +
+                     f"parameters with errors: \n{error_parameters}\n\n" +
+                     f"observations file: \n {observations_file_str_dict[obsservations_file_select]}\n\n" +
+                     f"number of iterations: \n {num_iterations} \n\n", 'settings')
+exp_handler.add_df(dataframe, 'data')
+exp_handler.add_marker_location(list_marker_locations[-1], 'last_marker_locations')
+exp_handler.save_experiment(r'/home/armin/catkin_ws/src/kident2/src/exp')
 
-print('fin')
---
+++ src/exp_data_handler.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+import datetime
+import pickle
+from da_test_suite_functions import latex_to_pdf
+class ExperimentDataHandler:
+    figures = {}
+    dataframes = {}
+    notes = {}
+    marker_locations = {}
+
+    def add_figure(self, figure, name):
+        self.figures[name] = figure
+
+    def add_df(self, df, name):
+        self.dataframes[name] = df
+
+    def add_note(self, note_body, note_name):
+        self.notes[note_name] = note_body
+
+    def add_marker_location(self, marker_locations, name):
+        self.marker_locations[name] = marker_locations
+
+    def save_figure(self, figure, savepath, name):
+        figure.canvas.draw()
+        figure.savefig(savepath.joinpath(name + '.png'))
+        with open(savepath.joinpath(name + '.p'), 'wb') as f_handle:
+            pickle.dump(figure, f_handle)
+
+    def save_experiment(self, path_string):
+        # prepare folder for saving data
+        path = Path(path_string)
+        timestamp = 'exp_' + str(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+        savepath = path.joinpath(timestamp)
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+
+        # save figures
+        for name in self.figures:
+            self.save_figure(self.figures[name], savepath, name)
+
+        # save notes
+        for name in self.notes:
+            with open(savepath.joinpath(name + '.txt'), 'w') as notefile:
+                notefile.write(self.notes[name])
+
+        # save data
+        for name in self.dataframes:
+            # for latex
+            cols = ['tex_names', 'errors_mm_deg', 'results_mm_deg', 'identification_accuracy']
+            headers = ['parameter', 'simuated', 'identified', 'difference']
+            texstr = self.dataframes[name].to_latex(escape=False, columns=cols, header=headers)
+            latex_to_pdf(savepath, 'data', texstr)
+
+            # as csv
+            self.dataframes[name].to_csv(savepath.joinpath('data.csv'))
+
+        # save marker locations
+        for mlocation in self.marker_locations:
+            with open(savepath.joinpath('marker_locations.p'), 'wb') as f:  # open a text file
+                pickle.dump(self.marker_locations[mlocation], f)  # serialize the list
+
